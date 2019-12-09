c304989e3d7b7ab76893d3319e9a85699647d1aa
shunsuke0519 <55390517+shunsuke0519@users.noreply.github.com>
2019-12-09 13:30:42 +0800

    first commit + assing key

--- src/App.js
+++ src/App.js
@@ -1,24 +1,45 @@
-import React from 'react';
-import logo from './logo.svg';
+import React,{useState} from 'react';
 import './App.css';
 
 function App() {
+
+  const [users, setUsers] = useState([
+    {
+      id: 1,
+      username: "blake",
+      profileImage:
+        "http://next-curriculum-instagram.s3.amazonaws.com/idol2-blake.jpg"
+    },
+    {
+      id: 2,
+      username: "ryanG",
+      profileImage:
+        "http://next-curriculum-instagram.s3.amazonaws.com/idol1-ryan.jpg"
+    },
+    {
+      id: 3,
+      username: "bigfan",
+      profileImage:
+        "http://next-curriculum-instagram.s3.amazonaws.com/bigfan-9AE7468E-4C35-41D1-AA68-31939891B5E1.png"
+    }
+  ]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Home Page</h1>
+      <ul>
+
+        {/* {users.map(user => (
+          <li> */}
+          {/* assing key */}
+        {users.map((user, index) => (
+          <li key={index}>
+
+            {user.id}: {user.profileImage}
+          </li>
+
+        ))}
+      </ul>
     </div>
   );
 }
